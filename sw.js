8e9d87bd91d223bc93607481ffeca39de8a9f007
devjhonatan08 <devjhonatan08@gmail.com>
2026-01-20 14:28:28 -0500

    ajustes en el service worker

--- sw.js
+++ sw.js
@@ -1,32 +1,35 @@
-const CACHE = "prestamo-pwa-v1";
-const ASSETS = [
-  "./",
-  "./index.html",
-  "./styles.css",
-  "./app.js",
-  "./manifest.json",
-];
+const CACHE = "prestamo-pwa-v2"; // CAMBIA VERSION
+const STATIC_ASSETS = ["./styles.css", "./manifest.json"];
 
+// INSTALACIÓN
 self.addEventListener("install", (e) => {
-  e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)));
+  e.waitUntil(caches.open(CACHE).then((cache) => cache.addAll(STATIC_ASSETS)));
   self.skipWaiting();
 });
 
+// ACTIVACIÓN
 self.addEventListener("activate", (e) => {
   e.waitUntil(
     caches
       .keys()
       .then((keys) =>
         Promise.all(
-          keys.filter((k) => k !== CACHE).map((k) => caches.delete(k))
-        )
-      )
+          keys.filter((k) => k !== CACHE).map((k) => caches.delete(k)),
+        ),
+      ),
   );
   self.clients.claim();
 });
 
+// FETCH
 self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((cached) => cached || fetch(e.request))
-  );
+  const req = e.request;
+
+  //  NO cachear JS ni HTML (lógica crítica)
+  if (req.url.endsWith(".js") || req.url.endsWith(".html")) {
+    return;
+  }
+
+  //  Cache solo assets estáticos
+  e.respondWith(caches.match(req).then((cached) => cached || fetch(req)));
 });
